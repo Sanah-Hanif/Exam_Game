98ed5ca84593067532668022e6b6d75ded0c507d
Sanah-Hanif <saneebanee@yahoo.com>
2019-08-26 17:09:32 +0200

    camera stuff

---
+++ Assets/Scripts/TemporaryScripts/JumpingScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpingScript : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+    public float maxMovementSpeed, acceleration, deceleration, jumpVelocity, fallMultiplier, lowJumpMultiplier;
+    private float _movementSpeed;
+    private bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGrounded = true;
+        _movementSpeed = 0;
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded)
+        {
+            _rb.velocity = Vector2.up * jumpVelocity;
+            isGrounded = false;
+        }
+        if (_rb.velocity.y < 0)
+        {
+            _rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        } else if (_rb.velocity.y > 0 && !Input.GetKey(KeyCode.UpArrow))
+        {
+            _rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+        if (Input.GetAxis("Horizontal") != 0 && _movementSpeed < maxMovementSpeed && _movementSpeed > -maxMovementSpeed)
+        { //controls movement left and right
+            _movementSpeed += Input.GetAxis("Horizontal") * Time.deltaTime * acceleration;
+        }
+        else if (Input.GetAxis("Horizontal") != 0 && _movementSpeed >= maxMovementSpeed)
+        { //moving right
+            _movementSpeed = maxMovementSpeed;
+        }
+        else if (Input.GetAxis("Horizontal") != 0 && _movementSpeed <= -maxMovementSpeed)
+        { //moving left
+            _movementSpeed = -maxMovementSpeed;
+        }
+
+        if (_movementSpeed > 0)
+            //deceletating when moving right
+            _movementSpeed += deceleration * Time.deltaTime;
+        if (_movementSpeed < 0)
+            //decelerating when moving left
+            _movementSpeed -= deceleration * Time.deltaTime;
+        if (_movementSpeed < 0.05 && _movementSpeed > -0.05)
+            //making sure that all adequately small speeds go down to zero
+            _movementSpeed = 0;
+
+        _rb.velocity = new Vector2(_movementSpeed, _rb.velocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        isGrounded = true;
+    }
+}
---
+++ Assets/Scripts/TemporaryScripts/LevelGenerator.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class ColourToPrefab
+{
+    public Color col;
+    public GameObject prefab;
+}
+
+public class LevelGenerator : MonoBehaviour
+{
+    public Texture2D level;
+    public ColourToPrefab[] entries;
+    public float startX, startY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateLevel();
+    }
+
+    void GenerateLevel ()
+    {
+        for (int i = 0; i < level.width; i++)
+        {
+            for (int j = 0; j < level.height; j++)
+            {
+                GenerateBlock(i, j);
+            }
+        }
+    }
+
+    void GenerateBlock(int x, int y)
+    {
+        Color myColour = level.GetPixel(x, y);
+        if (myColour.a == 0)
+            return;
+
+        foreach(ColourToPrefab entry in entries)
+        {
+            if (entry.col.Equals(myColour))
+            {
+                Vector2 pos = new Vector2 (startX + x, startY + y);
+                Instantiate(entry.prefab, pos, Quaternion.identity, transform);
+            }
+        }
+    }
+}
